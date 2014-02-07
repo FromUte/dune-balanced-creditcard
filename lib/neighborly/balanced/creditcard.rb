d3c3eaabafb0640bfa574d2e38c951330bd65acd
Josemar Luedke <josemarluedke@gmail.com>
2014-02-07 20:23:08 -0200

    Initialize gem

---
+++ lib/neighborly/balanced/creditcard.rb
@@ -0,0 +1,9 @@
+require "neighborly/balanced/creditcard/version"
+
+module Neighborly
+  module Balanced
+    module Creditcard
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/neighborly/balanced/creditcard/version.rb
@@ -0,0 +1,7 @@
+module Neighborly
+  module Balanced
+    module Creditcard
+      VERSION = "0.0.1"
+    end
+  end
+end
