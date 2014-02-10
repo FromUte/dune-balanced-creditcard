3f6cb92748cf6c287fb763a17ddcd94f4834d2d1
Josemar Luedke <josemarluedke@gmail.com>
2014-02-10 20:51:38 -0200

    Create new action, view and routes

--- app/controllers/neighborly/balanced/creditcard/payments_controller.rb
+++ app/controllers/neighborly/balanced/creditcard/payments_controller.rb
@@ -1,5 +1,6 @@
 module Neighborly::Balanced::Creditcard
   class PaymentsController < ActionController::Base
+    def new
+    end
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 Neighborly::Balanced::Creditcard::Engine.routes.draw do
+  resources :payments, only: [:new, :create]
 end
