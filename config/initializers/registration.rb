c5694eae6ccf305bfaebdffe06c48c6f8c92bacc
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-02-10 22:03:52 -0200

    Set contribution_id on query_string in payments routes

--- config/initializers/registration.rb
+++ config/initializers/registration.rb
@@ -1,9 +1,12 @@
-review_path = Neighborly::Balanced::Creditcard::Engine.
-                routes.url_helpers.new_payment_path(contribution)
+review_path = ->(contribution) do
+  Neighborly::Balanced::Creditcard::Engine.
+    routes.url_helpers.new_payment_path(contribution_id: contribution)
+end
+
 begin
   PaymentEngines.register(name:        'balanced-creditcard',
                           locale:      'en',
-                          review_path: ->(contribution) { review_path })
+                          review_path: review_path)
 rescue Exception => e
   puts "Error while registering payment engine: #{e}"
 end
