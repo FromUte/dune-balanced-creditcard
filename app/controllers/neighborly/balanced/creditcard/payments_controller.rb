3043d84ff12c9f3be23877d040701dc9ae802fe6
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-02-11 20:09:42 -0200

    Create Balanced's customer on PaymentsController#new action

--- app/controllers/neighborly/balanced/creditcard/payments_controller.rb
+++ app/controllers/neighborly/balanced/creditcard/payments_controller.rb
@@ -1,6 +1,13 @@
 module Neighborly::Balanced::Creditcard
   class PaymentsController < ActionController::Base
     def new
+      unless current_user.balanced_contributor
+        customer = Balanced::Customer.new(meta:  { user_id: current_user_id },
+                                          name:  current_user.name,
+                                          email: current_user.email)
+        customer.save
+        current_user.create_balanced_contributor(uri: customer.uri)
+      end
     end
   end
 end
--- spec/controllers/neighborly/balanced/creditcard/payments_controller_spec.rb
+++ spec/controllers/neighborly/balanced/creditcard/payments_controller_spec.rb
@@ -1,4 +1,30 @@
 require 'spec_helper'
 
 describe Neighborly::Balanced::Creditcard::PaymentsController do
+  describe "GET 'new'" do
+    let(:current_user) { stub_model(User) }
+    before do
+      controller.stub(:current_user).and_return(current_user)
+    end
+
+    context "when user already has a balanced_contributor associated" do
+      it "saves a new costumer" do
+        Balanced::Customer.any_instance.should_receive(:save)
+        get :new, contribution_id: 42
+      end
+
+      it "defines user_id in the meta data of the costumer" do
+        customer_attrs = hash_including(meta: hash_including(:user_id))
+        Balanced::Customer.should_receive(:new).with(customer_attrs)
+        get :new, contribution_id: 42
+      end
+    end
+
+    context "when user don't has balanced_contributor associated" do
+      it "skips creation of new costumer" do
+        Balanced::Customer.any_instance.should_receive(:save).never
+        get :new, contribution_id: 42
+      end
+    end
+  end
 end
