49f5884d273e19ffbe5ab69aebf89866e20f3773
Josemar Luedke <josemarluedke@gmail.com>
2014-02-12 17:54:43 -0200

    Extend user model from active record

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User
+class User < ActiveRecord::Base
   # TODO "Neighborly::" can be removed after the project rename to "Neighborly"
   has_one :balanced_contributor, class_name: 'Neighborly::Balanced::Creditcard::Contributor'
 end
