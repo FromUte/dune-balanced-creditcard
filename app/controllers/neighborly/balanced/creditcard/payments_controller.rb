1ec3a2c2d94e3740f044bc71b9d2852dd7fc1c5d
Josemar Luedke <josemarluedke@gmail.com>
2014-02-12 22:54:23 -0200

    Retrieve balanced customer informations if not new user and add address to new users

--- app/controllers/neighborly/balanced/creditcard/payments_controller.rb
+++ app/controllers/neighborly/balanced/creditcard/payments_controller.rb
@@ -1,12 +1,20 @@
 module Neighborly::Balanced::Creditcard
   class PaymentsController < ActionController::Base
     def new
-      unless current_user.balanced_contributor
-        customer = Balanced::Customer.new(meta:  { user_id: current_user.id },
-                                          name:  current_user.name,
-                                          email: current_user.email)
-        customer.save
-        current_user.create_balanced_contributor(uri: customer.uri)
+      if current_user.balanced_contributor
+        @customer = Balanced::Customer.find(current_user.balanced_contributor.uri)
+      else
+        @customer = Balanced::Customer.new(meta:   { user_id: current_user.id },
+                                          name:    current_user.display_name,
+                                          email:   current_user.email,
+                                          address: {
+                                                    line1:        current_user.address_street,
+                                                    city:         current_user.address_city,
+                                                    state:        current_user.address_state,
+                                                    postal_code:  current_user.address_zip_code
+                                                   })
+        @customer.save
+        current_user.create_balanced_contributor(uri: @customer.uri)
       end
     end
   end
