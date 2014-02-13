8b5c3799bba6b0cbfe1c8bbffc06f2bc90a8fda0
Josemar Luedke <josemarluedke@gmail.com>
2014-02-13 21:41:00 -0200

    Change balanced logging level to info on development

--- config/initializers/balanced.rb
+++ config/initializers/balanced.rb
@@ -1,1 +1,2 @@
-Balanced.configure(Configuration.fetch(:balanced_api_key_secret))
+options = Rails.env.development? ? { logging_level: 'INFO' } : {}
+Balanced.configure(Configuration.fetch(:balanced_api_key_secret), options)
