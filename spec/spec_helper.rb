9c234abcc11d659e4bea2bc8741da0000e6bba25
Josemar Luedke <josemarluedke@gmail.com>
2014-02-18 18:39:04 -0300

    Load the new dummy app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # Configure Rails Envinronment
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
 
 ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
