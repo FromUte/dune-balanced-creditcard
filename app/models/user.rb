b725f335d988cbdaa61b242c1d9118d4c98d6178
Josemar Luedke <josemarluedke@gmail.com>
2014-02-27 12:05:34 -0300

    Remove user model

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  # TODO "Neighborly::" can be removed after the project rename to "Neighborly"
-  has_one :balanced_contributor, class_name: 'Neighborly::Balanced::Creditcard::Contributor'
-end
