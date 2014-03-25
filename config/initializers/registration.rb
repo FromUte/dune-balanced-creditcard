8b5d0f0e92f33c578add6b9cb0f82949a0e6b1fa
Josemar Luedke <josemarluedke@gmail.com>
2014-03-25 16:04:36 -0300

    Use interface class for payment_engine

--- config/initializers/registration.rb
+++ config/initializers/registration.rb
@@ -1,17 +1,5 @@
-review_path = ->(contribution) do
-  Neighborly::Balanced::Creditcard::Engine.
-    routes.url_helpers.new_payment_path(contribution_id: contribution)
-end
-
-value_with_fees = ->(value) do
-  Neighborly::Balanced::Creditcard::TransactionAdditionalFeeCalculator.new(value).gross_amount
-end
-
 begin
-  PaymentEngine.register(name:             'balanced-creditcard',
-                          locale:          'en',
-                          value_with_fees: value_with_fees,
-                          review_path:     review_path)
+  PaymentEngine.new(Neighborly::Balanced::Creditcard::Interface.new).save
 rescue Exception => e
   puts "Error while registering payment engine: #{e}"
 end
--- lib/neighborly/balanced/creditcard.rb
+++ lib/neighborly/balanced/creditcard.rb
@@ -1,5 +1,6 @@
 require "neighborly/balanced/creditcard/engine"
 require "neighborly/balanced/creditcard/version"
+require "neighborly/balanced/creditcard/interface"
 
 module Neighborly
   module Balanced
