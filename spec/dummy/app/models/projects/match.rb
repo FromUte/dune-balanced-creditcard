0d9f5725160af480bea0cf2d657595722f006de1
Josemar Luedke <josemarluedke@gmail.com>
2014-04-25 11:07:29 -0300

    Create dummy match model

---
+++ spec/dummy/app/models/projects/match.rb
@@ -0,0 +1,31 @@
+class Projects::Match
+  extend ActiveModel::Naming
+
+  def self.find(*)
+    new
+  end
+
+  def self.find_by(*)
+    new
+  end
+
+  def cancel!(*);           end
+  def confirm!(*);          end
+  def update_attributes(*); end
+
+  def id(*)
+    42
+  end
+
+  def value(*)
+    50
+  end
+
+  def project
+    @project ||= Project.new
+  end
+
+  def user
+    @user ||= User.new
+  end
+end
