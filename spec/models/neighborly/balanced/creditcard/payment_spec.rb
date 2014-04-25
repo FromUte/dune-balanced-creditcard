cac386963f840cf14e2ec88b874f25e2b50241aa
Josemar Luedke <josemarluedke@gmail.com>
2014-04-25 11:18:10 -0300

    Rename contribution to resource and remove double quotes on payment model spec

--- spec/models/neighborly/balanced/creditcard/payment_spec.rb
+++ spec/models/neighborly/balanced/creditcard/payment_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Neighborly::Balanced::Creditcard::Payment do
   let(:customer)     { double('::Balanced::Customer') }
-  let(:contribution) { mock_model('Contribution', value: 1234).as_null_object }
+  let(:resource) { mock_model('Contribution', value: 1234).as_null_object }
   let(:debit)        { double('::Balanced::Debit').as_null_object }
   let(:attributes)   { { use_card: 'my-new-card' } }
   let(:project_owner_customer) do
@@ -12,33 +12,33 @@ describe Neighborly::Balanced::Creditcard::Payment do
   subject do
     described_class.new('balanced-creditcard',
                         customer,
-                        contribution,
+                        resource,
                         attributes)
   end
 
   before do
     ::Balanced::Customer.stub(:find).and_return(project_owner_customer)
-    contribution.stub_chain(:project, :user, :balanced_contributor).and_return(
+    resource.stub_chain(:project, :user, :balanced_contributor).and_return(
       double('BalancedContributor', uri: 'project-owner-uri'))
 
     described_class.any_instance.stub(:meta).and_return({})
   end
 
-  describe "contribution amount in cents" do
-    context "when customer is paying fees" do
+  describe 'amount in cents' do
+    context 'when customer is paying fees' do
       let(:attributes) { { pay_fee: '1', use_card: 'my-new-card' } }
 
-      it "returns gross amount from TransactionAdditionalFeeCalculator" do
+      it 'returns gross amount from TransactionAdditionalFeeCalculator' do
         Neighborly::Balanced::Creditcard::TransactionAdditionalFeeCalculator.
           any_instance.stub(:gross_amount).and_return(15)
         expect(subject.amount_in_cents).to eql(1500)
       end
     end
 
-    context "when customer is not paying fees" do
+    context 'when customer is not paying fees' do
       let(:attributes) { { pay_fee: '0', use_card: 'my-new-card' } }
 
-      it "returns gross amount from TransactionInclusiveFeeCalculator" do
+      it 'returns gross amount from TransactionInclusiveFeeCalculator' do
         Neighborly::Balanced::Creditcard::TransactionInclusiveFeeCalculator.
           any_instance.stub(:gross_amount).and_return(10)
         expect(subject.amount_in_cents).to eql(1000)
@@ -46,44 +46,44 @@ describe Neighborly::Balanced::Creditcard::Payment do
     end
   end
 
-  describe "checkout" do
-    shared_examples "updates contribution object" do
+  describe 'checkout' do
+    shared_examples 'updates resource object' do
       let(:attributes) { { pay_fee: '1', use_card: 'my-new-card' } }
 
-      it "debits customer on selected funding instrument" do
+      it 'debits customer on selected funding instrument' do
         customer.should_receive(:debit).
                  with(hash_including(source_uri: 'my-new-card')).
                  and_return(debit)
         subject.checkout!
       end
 
-      it "defines given engine's name as payment method of the contribution" do
-        contribution.should_receive(:update_attributes).
+      it 'defines given engine\'s name as payment method of the resource' do
+        resource.should_receive(:update_attributes).
                      with(hash_including(payment_method: 'balanced-creditcard'))
         subject.checkout!
       end
 
-      it "saves paid fees on contribution object" do
+      it 'saves paid fees on resource object' do
         calculator = double('FeeCalculator', fees: 0.42).as_null_object
         subject.stub(:fee_calculator).and_return(calculator)
-        contribution.should_receive(:update_attributes).
+        resource.should_receive(:update_attributes).
                      with(hash_including(payment_service_fee: 0.42))
         subject.checkout!
       end
 
-      it "saves who paid the fees" do
+      it 'saves who paid the fees' do
         calculator = double('FeeCalculator', fees: 0.42).as_null_object
         subject.stub(:fee_calculator).and_return(calculator)
-        contribution.should_receive(:update_attributes).
+        resource.should_receive(:update_attributes).
                      with(hash_including(payment_service_fee_paid_by_user: '1'))
         subject.checkout!
       end
     end
 
-    context "when customer is paying fees" do
+    context 'when customer is paying fees' do
       let(:attributes) { { pay_fee: '1', use_card: 'my-new-card' } }
 
-      it "debits customer with contribution amount in cents" do
+      it 'debits customer with amount in cents' do
         subject.stub(:amount_in_cents).and_return(1000)
         customer.should_receive(:debit).
                  with(hash_including(amount: 1000)).
@@ -92,19 +92,19 @@ describe Neighborly::Balanced::Creditcard::Payment do
       end
     end
 
-    context "with successful debit" do
+    context 'with successful debit' do
       before { customer.stub(:debit).and_return(debit) }
 
-      include_examples "updates contribution object"
+      include_examples 'updates resource object'
 
-      it "confirms the contribution" do
-        expect(contribution).to receive(:confirm!)
+      it 'confirms the resource' do
+        expect(resource).to receive(:confirm!)
         subject.checkout!
       end
 
-      it 'defines id as payment id of the contribution' do
+      it 'defines id as payment id of the resource' do
         debit.stub(:id).and_return('i-am-an-id!')
-        contribution.should_receive(:update_attributes).
+        resource.should_receive(:update_attributes).
                      with(hash_including(payment_id: 'i-am-an-id!'))
         subject.checkout!
       end
@@ -120,7 +120,7 @@ describe Neighborly::Balanced::Creditcard::Payment do
       end
 
       it 'defines description on debit' do
-        contribution.stub_chain(:project, :name).and_return('Awesome Project')
+        resource.stub_chain(:project, :name).and_return('Awesome Project')
         customer.should_receive(:debit).
                  with(hash_including(description: 'Contribution to Awesome Project')).
                  and_return(debit)
@@ -143,46 +143,46 @@ describe Neighborly::Balanced::Creditcard::Payment do
       end
     end
 
-    context "when raising Balanced::PaymentRequired exception" do
+    context 'when raising Balanced::PaymentRequired exception' do
       before do
         customer.stub(:debit).and_raise(Balanced::PaymentRequired.new({}))
       end
 
-      include_examples "updates contribution object"
+      include_examples 'updates resource object'
 
-      it "cancels the contribution" do
-        expect(contribution).to receive(:cancel!)
+      it 'cancels the resource' do
+        expect(resource).to receive(:cancel!)
         subject.checkout!
       end
     end
   end
 
-  describe "successful state" do
+  describe 'successful state' do
     before do
       customer.stub(:debit).and_return(debit)
     end
 
-    context "after checkout" do
+    context 'after checkout' do
       before { subject.checkout! }
 
-      it "is successfull when the debit has 'succeeded' status" do
+      it 'is successfull when the debit has \'succeeded\' status' do
         debit.stub(:status).and_return('succeeded')
         expect(subject).to be_successful
       end
 
-      it "is successfull when the debit has 'pending' status" do
+      it 'is successfull when the debit has \'pending\' status' do
         debit.stub(:status).and_return('pending')
         expect(subject).to be_successful
       end
 
-      it "is not successfull when the debit has others statuses" do
+      it 'is not successfull when the debit has others statuses' do
         debit.stub(:status).and_return('failed')
         expect(subject).to_not be_successful
       end
     end
 
-    context "before checkout" do
-      it "is not successfull" do
+    context 'before checkout' do
+      it 'is not successfull' do
         expect(subject).to_not be_successful
       end
     end
